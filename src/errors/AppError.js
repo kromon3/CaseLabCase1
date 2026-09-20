45f0e745392f6461b14eecb3e04fb383c248114d
kromon3 <kirillpley56@gmail.com>
2026-09-20 22:43:49 +0300

    feat: добавил ошибки api

--- src/errors/AppError.js
+++ src/errors/AppError.js
@@ -10,3 +10,23 @@ export class AppError extends Error {
 
 // Некорректные аргументы командной строки
 export class ValidationError extends AppError {}
+
+// Геокодинг ничего не нашёл
+export class NotFoundError extends AppError {}
+
+// API ответил кодом 4xx или 5xx
+export class ApiError extends AppError {
+  constructor(message, status) {
+    super(message);
+    this.status = status;
+  }
+}
+
+// Нет сети, DNS, отказ соединения
+export class NetworkError extends AppError {}
+
+// Сработал таймаут (AbortController)
+export class TimeoutError extends AppError {}
+
+// Ответ не разобрать: не JSON или неожиданная структура
+export class ParseError extends AppError {}
